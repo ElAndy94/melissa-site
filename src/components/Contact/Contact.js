f142c706d0f8ab161f25c0ca392d14f8450ce647
Andrew <andrewpeliza@hotmail.com>
2019-06-10 18:13:52 +0100

    Added all the pages required

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Contact.scss';
+
+function Contact() {
+  return (
+    <div className='home__container'>
+      <div>
+        <h1>Design is a language made visible.</h1>
+      </div>
+      <div>
+        <p>
+          God help us, we're in the hands of engineers. My dad once told me,
+          laugh and the world laughs with you.
+        </p>
+        <p>Cry, and I'll give you something to cry about you little bastard!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
